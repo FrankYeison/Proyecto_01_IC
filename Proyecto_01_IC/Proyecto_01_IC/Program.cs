c0beae9086b15fbdb923cd97917b53b1a9105575
FRANK TARRILLO <franktarrillo84@gmail.com>
2024-11-14 12:29:25 -0500

    Segunda Actualizacion

--- Proyecto_01_IC/Proyecto_01_IC/Program.cs
+++ Proyecto_01_IC/Proyecto_01_IC/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hola soy yo y tu quien eres");
+Console.WriteLine("Hola soy frank y tu");
+Console.WriteLine("Hola soy yo Rodrigo");
